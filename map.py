6e63de5536e7835ea2400015ce428b7f0a323646
Миха <mishakardanov@gmail.com>
2022-02-13 03:41:23 +0300

    ThirdTask

--- map.py
+++ map.py
@@ -10,9 +10,10 @@ class ApiYandex(QWidget, Ui_ApiYandex):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.coords = '40.692077,55.614980'
+        self.coords = [40.692077, 55.614980]
         self.z = 14
-        self.response = requests.get(f'https://static-maps.yandex.ru/1.x/?ll={self.coords}'
+        self.response = requests.get(f'https://static-maps.yandex.ru/1.x/?ll='
+                                     f'{str(self.coords[0])},{str(self.coords[1])}'
                                      f'&size=450,450&z={self.z}&l=sat')
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
@@ -27,15 +28,38 @@ class ApiYandex(QWidget, Ui_ApiYandex):
         if event.key() == Qt.Key_PageUp:
             self.z += 1
             self.do_picture()
+        if event.key() == Qt.Key_Up:
+            self.move_picture(event.key())
+        if event.key() == Qt.Key_Down:
+            self.move_picture(event.key())
+        if event.key() == Qt.Key_Left:
+            self.move_picture(event.key())
+        if event.key() == Qt.Key_Right:
+            self.move_picture(event.key())
 
     def do_picture(self):
-        self.response = requests.get(f'https://static-maps.yandex.ru/1.x/?ll={self.coords}'
+        self.response = requests.get(f'https://static-maps.yandex.ru/1.x/?ll='
+                                     f'{str(self.coords[0])},{str(self.coords[1])}'
                                      f'&size=450,450&z={self.z}&l=sat')
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(self.response.content)
         self.pixmap = QPixmap(self.map_file)
         self.map.setPixmap(self.pixmap)
+
+    def move_picture(self, button):
+        if button == Qt.Key_Up:
+            self.coords[1] += 0.0005
+            self.do_picture()
+        if button == Qt.Key_Down:
+            self.coords[1] -= 0.0005
+            self.do_picture()
+        if button == Qt.Key_Right:
+            self.coords[0] += 0.0005
+            self.do_picture()
+        if button == Qt.Key_Left:
+            self.coords[0] -= 0.0005
+            self.do_picture()
 
 
 if __name__ == '__main__':
